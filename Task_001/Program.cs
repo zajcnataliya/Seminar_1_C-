7b355c7871b156592dc22b0e82b9d87bef1c3b96
Nataliya <zajc-nataliya@yandex.ru>
2022-09-18 17:53:43 +0300

    Task 001 возведение в квадрат

---
+++ Task_001/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+// (число умноженное на само себя).
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int sqr = number * number;
+
+Console.WriteLine($"Результат квадрата числа {number} равен {sqr}");
+
+//Console.WriteLine("Квадрат числа" + number + " равен " +  sqr);
+
+Console.WriteLine("Квадрат числа " + number + " равен " +  Math.Pow(number,2));
+
