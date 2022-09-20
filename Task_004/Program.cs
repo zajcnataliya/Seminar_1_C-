16506d0786f337a83e5cd38b0b435c2fb8de0d36
Nataliya Zaytseva <N.Zaitseva@avelar-solar.com>
2022-09-20 16:24:28 +0300

    task 004 от -n до n

---
+++ Task_004/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+//2 -> " -2, -1, 0, 1, 2"
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());  // 5
+number = Math.Abs(number);  // для возможности вводить отрицательное число
+int number2 = number * -1;  // -5
+while (number2 <= number)   // -5<5 да
+{
+    Console.Write(number2 + ", ");
+    number2 = number2 + 1;
+}
+//Console.Write("\b\b"); имитируется нажатие backspace, удаляется запятая на конце
+Console.WriteLine();
